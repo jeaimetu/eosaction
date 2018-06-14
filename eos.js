0b9a687b6a5ab365cb07190bc099ac6ad78f37a0
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-06-14 18:46:28 +0900

    Update eos.js

--- eos.js
+++ eos.js
@@ -9,7 +9,7 @@ eos = Eos(config) // 127.0.0.1:8888
 
 eos.getInfo({}).then(result => {console.log(result)})
 
-for(i = 600000;i<681850;i++){
+for(i = 680000;i<681850;i++){
  eos.getBlock(i).then(result => {
  console.log(result.transactions[0].trx.transaction.actions[0]);
 });
